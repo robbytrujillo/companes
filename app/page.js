03a47193e54d675b1c58844946efecdc0b4d62d8
robbytrujillo <robbyikusuma88@gmail.com>
2025-06-10 15:09:06 +0700

    Introduction Companes

--- app/page.js
+++ app/page.js
@@ -17,10 +17,30 @@ export default function Home() {
 
       {/* Hero Image Start */}
       <div className="max-w-5xl mx-auto relative">
-        <Image src={DataImage.Hero} alt="Hero Image" />
+        <Image src={DataImage.Hero} alt="Hero Image" priority/>
         <HeroInfo />
       </div>
       {/* Hero Image End */}
+
+      {/* Service Start */}
+      <div>
+        <div>
+          <i className="ri-money-dollar-circle-fill ri-3x text-slate-700"></i>
+          <p className="font-semibold text-2xl/normal mb-2">Memberikan Harga Terbaik</p>
+          <p className="text-base/loose">lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed velit augue. Nam ultrices pellentesque nulla sed fringilla. Etiam at pharetra sapien, at vulputate enim. </p>
+        </div>
+        <div>
+          <i className="ri-service-fill ri-3x text-slate-700"></i>
+          <p className="font-semibold text-2xl/normal mb-2">Memberikan Harga Terbaik</p>
+          <p className="text-base/loose">lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed velit augue. Nam ultrices pellentesque nulla sed fringilla. Etiam at pharetra sapien, at vulputate enim. </p>
+        </div>
+        <div>
+          <i className="ri-money-dollar-circle-fill ri-3x text-slate-700"></i>
+          <p className="font-semibold text-2xl/normal mb-2">Memberikan Harga Terbaik</p>
+          <p className="text-base/loose">lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed velit augue. Nam ultrices pellentesque nulla sed fringilla. Etiam at pharetra sapien, at vulputate enim. </p>
+        </div>
+      </div>
+      {/* Service End */}
     </>
     
   );
--- components/HeroInfo.jsx
+++ components/HeroInfo.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const HeroInfo = () => {
   return (
-    <div className="absolute bottom-0 left-0 text-white">
-      Hero Info
+    <div className="sm:bsolute bottom-0 left-0 sm:bg-slate-700/75 sm:text-white grid sm:grid-cols-3 w-full text-center h-56 items-center bg-transparent text-slate-700 sm:mt-0 mt-16 grid-cols-2 sm:gap-0 gap-7">
+      <div>
+        <p className="text-5xl font-bold">80+</p>
+        <h2 className="mt-3 text-xl">Project Selesai</h2>
+      </div>
+      <div>
+        <p className="text-5xl font-bold">15+</p>
+        <h2 className="mt-3 text-xl">Mitra</h2>
+      </div>
+      <div>
+        <p className="text-5xl font-bold">140+</p>
+        <h2 className="mt-3 text-xl">Karyawan</h2>
+      </div>
     </div>
   )
 }
