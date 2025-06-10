adc0b5e3f1bd33a3488d98ffc8f39ba31da92a13
robbytrujillo <robbyikusuma88@gmail.com>
2025-06-11 05:36:10 +0700

    Introduction Companes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,9 +5,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="container mx-auto px-4 w-full max-w-7xl">
-        <div className="pt-20 pb-6">
+        <div className="pt-20 pb-6 flex items-center justify-between sm:flex-row flex-col sm:gap-0 gap-4">
             <p>&copy; Copyright 2025 <span>Robby Ilham</span></p>
-            <div>
+            <div className="flex gap-4">
                 <Link href={"#"} target={"_blank"} className="text-slate-700 hover:text-slate-600">Terms</Link>
                 <Link href={"#"} target={"_blank"} className="text-slate-700 hover:text-slate-600">Privacy</Link>
                 <Link href={"#"} target={"_blank"} className="text-slate-700 hover:text-slate-600">Cookies</Link>
